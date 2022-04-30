50a671ac47bba03a215717be3a0ccefd12248a12
idrisskhaled1 <idrisskhaled@insat.u-carthage.tn>
2022-04-30 13:10:24 +0100

    ho

--- src/main/java/HO.java
+++ src/main/java/HO.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
@@ -11,18 +12,23 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
 public class HO {
     java.sql.Connection connection = null;
-
+    JFrame frame;
+    JPanel panel;
+    String data[][] = new String[10][7];
     public void retrieve(){
         try {
             Statement statement = connection.createStatement();
             ResultSet results = statement.executeQuery("SELECT * FROM `product sales`");
+            int i=0;
             while (results.next()) {
-                String data = results.getString(1);
-                System.out.println("Fetching data by column index for row " + results.getRow() + " : " + data);
-                data = results.getString(2);
-                System.out.println("Fetching data by column name for row " + results.getRow() + " : " + data);
+                data[i]= new String[]{results.getString(1),results.getString(2),results.getString(3),results.getString(4),results.getString(6),results.getString(7),results.getString(8)};
+                i++;
             }
         } catch (SQLException e) {
             System.out.println("Could not retrieve data from the database " + e.getMessage());
@@ -30,20 +36,32 @@ public class HO {
     }
     public void update(JSONObject message){
         try {
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery("UPDATE `product sales` set ");
-            //code here
+            PreparedStatement stmt = connection.prepareStatement("UPDATE `product sales` SET `Date`=?, `Region`=?, `Product`=?, `deletedAt`=?, `Qty`=?, `Cost`=?, `Amt`=? ");
+            stmt.setString(2, (String) message.get("region"));
+            stmt.setString(3, (String) message.get("product"));
+            stmt.setDate(1, (Date) message.get("date"));
+            stmt.setDate(4, (Date) message.get("deletedAt"));
+            stmt.setInt(5, (Integer) message.get("qty"));
+            stmt.setFloat(6,(Float)message.get("cost"));
+            stmt.setFloat(7,(Float)message.get("amt"));
+            stmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Could not retrieve data from the database " + e.getMessage());
+            System.out.println("Could not update data" + e.getMessage());
         }
     }
     public void insert(JSONObject message){
         try {
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery("INSERT INTO `product sales`(`Date`, `Region`, `Product`, `deletedAt`, `Qty`, `Cost`, `Amt`) VALUES('') ");
-            //code here
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO `product sales`(`Date`, `Region`, `Product`, `deletedAt`, `Qty`, `Cost`, `Amt`) VALUES (?,?,?,?,?,?,?)");
+            stmt.setString(2, (String) message.get("region"));
+            stmt.setString(3, (String) message.get("product"));
+            stmt.setDate(1, (Date) message.get("date"));
+            stmt.setDate(4, (Date) message.get("deletedAt"));
+            stmt.setInt(5, (Integer) message.get("qty"));
+            stmt.setFloat(6,(Float)message.get("cost"));
+            stmt.setFloat(7,(Float)message.get("amt"));
+            stmt.executeUpdate();
         } catch (SQLException e) {
-            System.out.println("Could not retrieve data from the database " + e.getMessage());
+            System.out.println("Could not insert data" + e.getMessage());
         }
     }
     public void delete(JSONObject message){
@@ -51,7 +69,7 @@ public class HO {
             Statement statement = connection.createStatement();
             ResultSet results = statement.executeQuery("DELETE from `product sales` WHERE id="+message.get("id"));
         } catch (SQLException e) {
-            System.out.println("Could not retrieve data from the database " + e.getMessage());
+            System.out.println("Could not delete data" + e.getMessage());
         }
     }
     public void connection(){
@@ -101,9 +119,24 @@ public class HO {
     }
     public static void main(String[] args) {
         new HO();
+
     }
     public HO(){
         connection();
         retrieve();
+        frame = new JFrame("HO");
+        panel = new JPanel();
+        String col[] = {"Id","Date","Region","Product","Qty","Cost","Amt"};
+        JTable table = new JTable(data,col);
+        JScrollPane pane = new JScrollPane(table);
+        pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        panel.add(pane);
+        frame.add(panel);
+        frame.setSize(500,150);
+        frame.setUndecorated(true);
+        frame.getRootPane().setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 }
